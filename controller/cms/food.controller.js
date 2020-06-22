2f99ac7aa98bb658c71277dcd961ec519541cd92
Ngoctrong102 <trong.vo102@hcmut.edu.vn>
2020-06-22 10:33:49 +0700

    render ra bang du lieu do an

--- controller/cms/food.controller.js
+++ controller/cms/food.controller.js
@@ -1,14 +1,12 @@
 var DB = require("../db");
-var ejs = require('ejs');
-var  fs = require('fs');
 
 module.exports = {
     index: (req,res)=>{
         DB.query('SELECT * FROM foods WHERE trash = 0', 
         function(err,results,fields){
             if (err) throw err;
-            var data = JSON.stringify(results.map((result,index)=>{return [index+1,result.image,result.title,result.price]}));
-            console.log(data);
+            var data = JSON.stringify(results.map((result,index)=>{return [index+1,result.image,result.title,result.price,123]}));
+            console.log(Array.isArray(data));
             res.render('cms/main_layout',{content:"foods/index",footer:'foods/footer',datatb:data});
         })
     }
