b3cfd1548de50d818b6c59bd96462520344fb727
Ngoctrong102 <trong.vo102@hcmut.edu.vn>
2020-06-23 10:30:10 +0700

    xong foods

--- controller/cms/food.controller.js
+++ controller/cms/food.controller.js
@@ -1,5 +1,5 @@
 var DB = require("../db");
-
+var fs = require('fs');
 module.exports = {
     index: (req, res) => {
         DB.query('SELECT * FROM foods WHERE trash = 0 ORDER BY created_date DESC',
@@ -10,7 +10,7 @@ module.exports = {
     },
     add: (req, res) => {
         console.log("req:", req.file);
-        var sql = `INSERT INTO foods (title, image, price, description, in_menu, created_date) VALUES ('${req.body.title}','${req.path}',${req.body.price},'${req.body.description}',${req.body.in_menu == "on" ? 1 : 0},NOW())`;
+        var sql = `INSERT INTO foods (title, image, price, description, in_menu, created_date) VALUES ('${req.body.title}','${req.file.filename}',${req.body.price},'${req.body.description}',${req.body.in_menu == "on" ? 1 : 0},NOW())`;
         DB.query(sql,
             function (err, results, fields) {
                 if (err) throw err;
@@ -34,11 +34,32 @@ module.exports = {
             })
     },
     update: (req,res)=>{
-        DB.query(`UPDATE foods SET title='${req.body.title}', price=${req.body.price}, description='${req.body.description}', in_menu=${req.body.in_menu ? 1 : 0} WHERE id=${req.params.id}`)
-        res.redirect('/cms/foods');
+        DB.query(`SELECT * FROM foods WHERE id=${req.params.id}`,
+        function(err, results, fields){
+            if (err) throw err;
+            var image = "";
+            if (req.file){
+                var path = 'public/uploads/foods/'+results[0].image;
+                if (fs.existsSync('public/uploads/foods/'+results[0].image)){
+                    fs.unlinkSync('public/uploads/foods/'+results[0].image)
+                }
+                image = req.file.filename;
+            }  else image = results[0].image;
+            DB.query(`UPDATE foods SET title='${req.body.title}',image='${image}', price=${req.body.price}, description='${req.body.description}', in_menu=${req.body.in_menu ? 1 : 0} WHERE id=${req.params.id}`)
+            res.redirect('/cms/foods');
+        })
     },
-    delete: (req,res)=>{
-        DB.query(`DELETE FROM foods WHERE id=${req.params.id}`);
+    delete: async (req,res)=>{
+        DB.query(`SELECT * FROM foods WHERE id=${req.params.id}`,
+        function(err,results,fields){
+            if (err) throw err;
+            if (results.length>0)
+            if (fs.existsSync('public/uploads/foods/'+results[0].image)){
+                fs.unlinkSync('public/uploads/foods/'+results[0].image)
+            }
+            DB.query(`DELETE FROM foods WHERE id=${req.params.id}`);
+        });
+        
         res.redirect('/cms/foods');
     }
 }
